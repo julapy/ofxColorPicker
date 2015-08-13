230be4425690ddc1d0ce2ca78810f9a3256cd88f
lukasz karluk <julapy@googlemail.com>
2015-08-14 08:05:58 +1000

    ofApp tweaks.

--- example/src/ofApp.cpp
+++ example/src/ofApp.cpp
@@ -12,100 +12,88 @@
 //	INIT.
 ///////////////////////////////////////////
 
-void ofApp::setup()
-{
-	ofSetFrameRate( frameRate = 60 );
-	ofSetVerticalSync( true );
+void ofApp::setup() {
+    
+	ofSetFrameRate(60);
 	
-	colorPicker0.setColorRadius( 1.0 );
-	colorPicker1.setColorRadius( 1.0 );
-	
-	colorPicker0.setColorAngle( 0.5 );
-	colorPicker1.setColorAngle( 0.83 );
+	colorPicker0.setColorRadius(1.0);
+    colorPicker0.setColorAngle(0.5);
+
+    colorPicker1.setColorRadius(1.0);
+	colorPicker1.setColorAngle(0.83);
 }
 
 ///////////////////////////////////////////
 //	UPDATE.
 ///////////////////////////////////////////
 
-void ofApp::update()
-{
-	float ang;
-	ang += frameRate * 0.00002;
+void ofApp::update() {
 
-	colorPicker1.setColorAngle( colorPicker1.getColorAngle() + ang );
+    float angle = colorPicker1.getColorAngle();
+	angle += ofGetLastFrameTime() * 0.03;
+
+	colorPicker1.setColorAngle(angle);
 	
 	colorPicker0.update();
 	colorPicker1.update();
 	
-	rect.setCornerColor( colorPicker0.getColor(), 0 );
-	rect.setCornerColor( colorPicker0.getColor(), 1 );
-	rect.setCornerColor( colorPicker1.getColor(), 2 );
-	rect.setCornerColor( colorPicker1.getColor(), 3 );
+	rect.setCornerColor(colorPicker0.getColor(), 0);
+	rect.setCornerColor(colorPicker0.getColor(), 1);
+	rect.setCornerColor(colorPicker1.getColor(), 2);
+	rect.setCornerColor(colorPicker1.getColor(), 3);
 }
 
 ///////////////////////////////////////////
 //	DRAW.
 ///////////////////////////////////////////
 
-void ofApp::draw()
-{
-	rect.draw();
-	
-	//--
+void ofApp::draw() {
 
-	int x, y, w, h, g;
-
-	w = 150;
-	h = 300;
-	x = 20;
+    rect.draw();
 	
-	g = (int)( ( ofGetHeight() - h * 2 ) / 3 );		// gap.
+    int x = 20;
+    int y = 0;
+    int w = 150;
+    int h = 300;
+	int g = (int)((ofGetHeight() - h * 2) / 3); // gap.
 	y = g;
 	
-	colorPicker0.draw( x, y, w, h );
+	colorPicker0.draw(x, y, w, h);
 	
 	y = y + h + g;
 	
-	colorPicker1.draw( x, y, w, h );
+	colorPicker1.draw(x, y, w, h);
 }
 
 ///////////////////////////////////////////
 //	HANDLERS.
 ///////////////////////////////////////////
 
-void ofApp::keyPressed(int key)
-{
-
+void ofApp::keyPressed(int key) {
+    //
 }
 
-void ofApp::keyReleased(int key)
-{
-
+void ofApp::keyReleased(int key) {
+    //
 }
 
-void ofApp::mouseMoved(int x, int y )
-{
-
+void ofApp::mouseMoved(int x, int y ) {
+    //
 }
 
-void ofApp::mouseDragged(int x, int y, int button)
-{
-
+void ofApp::mouseDragged(int x, int y, int button) {
+    //
 }
 
-void ofApp::mousePressed(int x, int y, int button)
-{
-
+void ofApp::mousePressed(int x, int y, int button) {
+    //
 }
 
-void ofApp::mouseReleased(int x, int y, int button)
-{
-
+void ofApp::mouseReleased(int x, int y, int button) {
+    //
 }
 
-void ofApp::windowResized(int w, int h)
-{
-
+void ofApp::windowResized(int w, int h) {
+    //
 }
 
